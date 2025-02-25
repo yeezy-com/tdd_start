0b7dfa9dd117d73052e75cb4e939f8eaf8d3fb94
GiSeok Kang <ime.rokz@gmail.com>
2025-02-25 22:41:41 +0900

    test: 만원 납부 시 만료일을 계산하는 테스트 코드 성공

--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -5,6 +5,6 @@ import java.time.LocalDate;
 public class ExpiryDateCalculator {
 
     public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
-        return LocalDate.of(2024, 4, 1);
+        return billingDate.plusMonths(1);
     }
 }
