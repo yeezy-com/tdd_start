c07de20e09502ad784570599cf5228c1e0afe489
GiSeok Kang <ime.rokz@gmail.com>
2025-02-25 22:45:25 +0900

    refactor: 만원 납부 시 만료일을 계산하는 코드 중복 제거

--- src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -9,20 +9,22 @@ public class ExpiryDateCalculatorTest {
     // 쉬운 것부터 테스트하자.
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨() {
-        LocalDate billingDate = LocalDate.of(2024, 3, 1);
-        int payAmount = 10_000;
+        assertExpiryDate(
+                LocalDate.of(2024, 3, 1),
+                10_000,
+                LocalDate.of(2024, 4, 1)
+        );
+        assertExpiryDate(
+                LocalDate.of(2024, 5, 5),
+                10_000,
+                LocalDate.of(2024, 6, 5)
+        );
+    }
 
+    private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
         ExpiryDateCalculator cal = new ExpiryDateCalculator();
-        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        LocalDate realExpiryDate = cal.calculateExpiryDate(billingDate, payAmount);
 
-        Assertions.assertEquals(LocalDate.of(2024, 4, 1), expiryDate);
-
-        LocalDate billingDate2 = LocalDate.of(2024, 5, 5);
-        int payAmount2 = 10_000;
-
-        ExpiryDateCalculator cal2 = new ExpiryDateCalculator();
-        LocalDate expiryDate2 = cal.calculateExpiryDate(billingDate2, payAmount);
-
-        Assertions.assertEquals(LocalDate.of(2024, 6, 5), expiryDate2);
+        Assertions.assertEquals(expectedExpiryDate, realExpiryDate);
     }
 }
