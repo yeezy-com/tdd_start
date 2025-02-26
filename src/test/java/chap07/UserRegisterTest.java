821bc93b465a3633a4cdc50d2e46642cd7b961fa
GiSeok Kang <ime.rokz@gmail.com>
2025-02-26 19:14:59 +0900

    red: 약한 암호 가입 실패 테스트

---
+++ src/test/java/chap07/UserRegisterTest.java
@@ -0,0 +1,26 @@
+package chap07;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserRegisterTest {
+    private UserRegister userRegister;
+    private StubWeakPasswordChecker stubPasswordChecker = new StubWeakPasswordChecker();
+
+    @BeforeEach
+    void setUp() {
+        userRegister = new UserRegister(stubPasswordChecker);
+    }
+
+    @DisplayName("약한 암호면 가입 실패")
+    @Test
+    void weakPassword() {
+        stubPasswordChecker.setWeak(true); // 암호가 약하다고 응답하도록 설정
+
+        Assertions.assertThrows(WeakPasswordException.class, () -> {
+            userRegister.register("id", "pw", "email");
+        });
+    }
+}
