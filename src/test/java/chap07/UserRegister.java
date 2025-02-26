b699ad69e545beb5745eb2376474b70b60d814f4
GiSeok Kang <ime.rokz@gmail.com>
2025-02-26 20:39:38 +0900

    refactor: 같은 ID가 없으면 가입 성공 구현 일반화

--- src/test/java/chap07/UserRegister.java
+++ src/test/java/chap07/UserRegister.java
@@ -18,6 +18,6 @@ public class UserRegister {
         if (user != null) {
             throw new DupIdException();
         }
-        userRepository.save(new User("id", "pw", "email"));
+        userRepository.save(new User(id, pw, email));
     }
 }
