df08a1aecb55014c66155fd1cfc3e3c2703deb95
GiSeok Kang <ime.rokz@gmail.com>
2025-02-26 10:31:27 +0900

    red: 첫 납부일 이후 만료일 전에 만원 납부 시 한달 연장되는 테스트 추가

--- src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -59,6 +59,14 @@ public class ExpiryDateCalculatorTest {
                 .build();
 
         assertExpiryDate(payData, LocalDate.of(2019, 3, 31));
+
+        PayData payData2 = PayData.builder()
+                .firstBillingDate(LocalDate.of(2019, 1, 30))
+                .billingDate(LocalDate.of(2019, 2, 28))
+                .payAmount(10_000)
+                .build();
+
+        assertExpiryDate(payData2, LocalDate.of(2019, 3, 30));
     }
 
     private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
